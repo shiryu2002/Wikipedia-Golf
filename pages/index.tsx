7eb74ecbbf2dc5c4b52e24d0b1d6c16072956c0c
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2024-01-06 04:10:04 +0900

    URLの変更時にイベントリスナーを更新するためのコードを追加しました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import { useEffect } from "react";
 export default function Home() {
+  const [baseURL, setBaseURL] = useState<string>(
+    "https://ja.wikipedia.org/wiki/"
+  );
   const [url, setUrl] = useState<string>("メインページ");
   const [stroke, setStroke] = useState<number>(0);
+  const [history, setHistory] = useState<{ url: string; stroke: number }[]>([]);
 
   useEffect(() => {
     const iframe = document.querySelector("iframe");
+    console.log(iframe);
     if (iframe) {
+      console.table(history);
       const handleLoad = () => {
+        //記事名を取得
         setStroke((prevStroke) => prevStroke + 1);
       };
       iframe.addEventListener("load", handleLoad);
@@ -17,22 +24,18 @@ export default function Home() {
         iframe.removeEventListener("load", handleLoad);
       };
     }
-  }, [url]); // URLが変更されるたびにイベントリスナーを更新
+  }, [history, stroke]);
 
   return (
     <main>
       <div>
-        <p className="text-center text-3xl mt-6 font-bold">打数: {stroke}</p>
-        <input
-          className={`border-2 border-gray-300 rounded-md p-2 m-2 text-black`}
-          type="text"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
+        <p className="text-center text-3xl my-6 ">
+          打数:<span className="text-5xl font-bold"> {stroke}</span>
+        </p>
       </div>
       <iframe
         className={`h-screen w-screen`}
-        src={`https://jp.wikipedia.org/wiki/${url}`}
+        src={`${baseURL}${url}`}
         title="Wikipedia Page"
       ></iframe>
     </main>
